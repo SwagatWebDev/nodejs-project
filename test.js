9e599949ee3945d510c2dd5a4f5589123c9ca007
swagat030 <swagatkumarmishra888@gmail.com>
2024-01-25 23:57:48 +0530

    file updated

--- test.js
+++ test.js
@@ -1,40 +1,9 @@
-private String extractTextBetween(String text, char startDelimiter, char endDelimiter) {
-    if (text != null && text.contains(String.valueOf(startDelimiter)) && text.contains(String.valueOf(endDelimiter))) {
-        int start = text.indexOf(startDelimiter) + 1;
-        int end = text.indexOf(endDelimiter);
+private String removeContentInsideParentheses(String text) {
+    if (text != null && text.contains("(") && text.contains(")")) {
+        int start = text.indexOf("(");
+        int end = text.indexOf(")");
 
-        return (start < end) ? text.substring(start, end) : text;
+        return text.substring(0, start) + text.substring(end + 1);
     }
     return text;
-}
-
-private String extractTextBetweenSquareBrackets(String text) {
-    return extractTextBetween(text, '[', ']');
-}
-
-private String extractTextBetweenParentheses(String text) {
-    return extractTextBetween(text, '(', ')');
-}
-
-private String extractValueFromBusinessCapability(String text) {
-    if (text != null) {
-        Pattern pattern = Pattern.compile("BusinessCapability : \"(.*?)\"");
-        Matcher matcher = pattern.matcher(text);
-
-        return matcher.find() ? matcher.group(1) : text;
-    }
-    return text;
-}
-
-private String getProcessedNewValue(String newValue, String eventType) {
-    switch (eventType) {
-        case "New":
-            return extractTextBetweenSquareBrackets(newValue);
-        case "Disposition":
-            return extractTextBetweenParentheses(newValue);
-        case "Capability":
-            return extractValueFromBusinessCapability(newValue);
-        default:
-            return newValue;
-    }
 }
