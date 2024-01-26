07d24712f27edbbdb9da293da79cb8e83da15865
swagat030 <swagatkumarmishra888@gmail.com>
2024-01-26 17:04:20 +0530

    file updated

--- index.js
+++ index.js
@@ -2,157 +2,48 @@
 
 const express = require('express');
 
+const fs = require('fs');
+
 const server = express();
 
 const morgan = require('morgan');
 
 const port = 9000;
 
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 
+const products = data.products;
+
+server.use(express.json())
 server.use(morgan('dev'));
+
+//CRUD
+
+// http://localhost:8000/products
+
+// http://localhost:8000 / http://localhost:8000/api/v1 - API Route, base URL
+
+
+// GET Request
+const baseURL = "/api/v1";
+
+server.post(baseURL+"/products", (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.status(201).json(products);
+})
+server.get(baseURL+'/products', (req, res) => {
+    res.json(products);
+})
+
+server.get(baseURL+'/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const product = products.find(product => product.id === id);
+    res.json(product);
+})
 
 server.listen(port, () => {
    console.log(`Express Server running on http://localhost:${port}`);
  });
-
-
-// // 4. Built in Middleware
-//
-// const express = require('express');
-//
-// const server = express();
-//
-//
-//
-// const port = 9000;
-//
-// server.use(express.static('public'));
-//
-// // Body parser
-// server.use(express.json());
-//
-// server.listen(port, () => {
-//     console.log(`Express Server running on http://localhost:${port}`);
-// });
-
-
-// 3. Error Handling Middleware
-// const express = require('express');
-//
-// const server = express();
-//
-// const port = 9000;
-//
-// server.get('/', (req, res, next) => {
-//     const err = new Error('Custom Error');
-//     next(err);
-// });
-//
-// server.use((err, req, res, next) => {
-//     console.log(err.stack);
-//     res.status(500).send('Something Went Wrong');
-// })
-//
-// server.listen(port, () => {
-//      console.log(`Express Server running on http://localhost:${port}`);
-//  });
-
-// 1. Creating auth in Application Level / Routing Level Middleware
-
-// const express = require('express');
-//
-// const server = express();
-//
-// const port = 9000;
-//
-// // const auth = (req, res, next) => {
-// //     console.log(req.query);
-// //     if(req.query.password === '123'){
-// //         next();
-// //     } else{
-// //         res.sendStatus(401);
-// //     }
-// // }
-//
-// //server.use(auth);
-//
-// // server.use((req, res, next) => {
-// //     console.log('Middleware function 1 executed');
-// //     console.log(req.method, req.ip, req.hostname, new Date());
-// //     next();
-// // });
-// //
-// // server.use((req, res, next) => {
-// //     console.log('Middleware function 2 executed');
-// //     console.log(`Date: ${new Date().toLocaleDateString()}`);
-// // })
-//
-//
-// // server.get('/', auth, (req, res) => {
-// //     //res.send('<h1>Hello</h1>');
-// //     res.json({type: 'GET'});
-// // });
-//
-// server.use(express.json());
-//
-// // const auth = (req, res, next) => {
-// //     console.log(req.body);
-// //     if(req.body.password === '123'){
-// //         next();
-// //     } else{
-// //         res.sendStatus(401);
-// //     }
-// // }
-// //
-// // server.post('/', auth, (req, res) => {
-// //     //res.send('<h1>Hello</h1>');
-// //     res.json({type: 'POST'});
-// // });
-//
-// server.put('/', (req, res) => {
-//     //res.send('<h1>Hello</h1>');
-//     res.json({type: 'PUT'});
-// });
-//
-// server.delete('/', (req, res) => {
-//     //res.send('<h1>Hello</h1>');
-//     res.json({type: 'DELETE'});
-// });
-//
-// server.patch('/', (req, res) => {
-//     //res.send('<h1>Hello</h1>');
-//     res.json({type: 'PATCH'});
-// });
-//
-// server.listen(port, () => {
-//     console.log(`Express Server running on http://localhost:${port}`);
-// });
-
-
-// 2. Routing Level Middleware:
-
-// const express = require('express');
-// const app = express();
-// const router = express.Router();
-//
-// // Router-level middleware
-// router.use((req, res, next) => {
-//     console.log('Router-level middleware');
-//     next();
-// });
-//
-// // Route
-// router.get('/special', (req, res) => {
-//     res.send('Special Route');
-// });
-//
-// // Using the router in the main app
-// app.use('/api', router);
-//
-// // Start the server
-// const PORT = process.env.PORT || 9000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
 
 
