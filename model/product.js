8a97608c09fe3e7ff286b99fe441506fa2cd35e6
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-11 06:37:18 +0530

    crud example updated with details

--- model/product.js
+++ model/product.js
@@ -11,6 +11,7 @@ const productSchema = new Schema({
     brand: {type: String, required: true},
     category: {type: String, required: true},
     thumbnail: {type: String, required: true},
+    images: [String]
 })
 
 // productSchema.set('toJSON', { virtual: true, versionKey: false,
@@ -18,55 +19,3 @@ const productSchema = new Schema({
 //     {delete ret._id}} );
 
 exports.Product = mongoose.model('Product', productSchema);
-
-/*
-User:
-[
-  { _id: 1, name: 'John', age: 30 },
-  { _id: 2, name: 'Alice', age: 25 },
-  { _id: 3, name: 'Bob', age: 35 },
-  { _id: 4, name: 'Emily', age: 40 },
-  { _id: 5, name: 'Charlie', age: 28 }
-]
-
-orders:
-[
-  { _id: ObjectId('65c76a8429f92520bbfd237c'), userId: 1, amount: 100 },
-  { _id: ObjectId('65c76a8429f92520bbfd237d'), userId: 2, amount: 150 },
-  { _id: ObjectId('65c76a8429f92520bbfd237e'), userId: 1, amount: 200 },
-  { _id: ObjectId('65c76a8429f92520bbfd237f'), userId: 3, amount: 120 },
-  { _id: ObjectId('65c76a8429f92520bbfd2380'), userId: 4, amount: 180 }
-]
-db.users.aggregate([
-{
-  $lookup: {
-    from: "orders",
-    localField: "_id",
-    foreignField: "userId",
-    as: "user_orders"
-  }
- },
- { $group: { _id: null, maxAmount: { $max: "$amount" } } }
-])
-
-db.orders.aggregate([
-  { $group: { _id: "$userId", totalRevenue: { $sum: "$amount" } } },
-  {
-    $lookup: {
-      from: "users",
-      localField: "_id",
-      foreignField: "_id",
-      as: "user_info"
-    }
-  }
-])
-
-db.orders.aggregate([
-  { $group: { _id: "$userId", totalOrders: { $sum: 1 } } },
-  { $sort: {totalOrders: -1 } }])
-
-
-
-*/
-
-
