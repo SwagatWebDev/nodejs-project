c09beb980ac6acfdc36c4a7c004ee84f90754914
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-19 09:58:26 +0530

    file updated

--- index.js
+++ index.js
@@ -13,26 +13,23 @@ const productRouter = require('./router/product');
 const userRouter = require('./router/user');
 const orderRouter = require('./router/order');
 const netflixUserRouter = require('./router/netflix-user');
-const {Schema} = require("mongoose");
-const {getUsersWithOrders} = require("./controller/user");
-const {getOrderFilter} = require("./controller/order");
+const { getUsersWithOrders } = require("./controller/user");
+const { getOrderFilter } = require("./controller/order");
 const cors = require('cors');
 
 // Server Middleware
-// server.use(cors());
+server.use(cors());
 server.use(express.json());
-server.use(baseURL+productURL, productRouter.router);
-server.use(baseURL+userURL, userRouter.router);
-server.use(baseURL+orderURL, orderRouter.router);
+server.use(baseURL + productURL, productRouter.router);
+server.use(baseURL + userURL, userRouter.router);
+server.use(baseURL + orderURL, orderRouter.router);
 console.log('DB Password', process.env.DB_PASSWORD);
 
-server.use(baseURL+ '/user-with-order', getUsersWithOrders);
+server.use(baseURL + '/user-with-order', getUsersWithOrders);
 
-server.use(baseURL+netflixUserURL, netflixUserRouter.router);
+server.use(baseURL + netflixUserURL, netflixUserRouter.router);
 
 // starting server
 server.listen(process.env.PORT, () => {
-   console.log(`Express Server running on http://localhost:${process.env.PORT}`);
- });
-
-
+    console.log(`Express Server running on http://localhost:${process.env.PORT}`);
+});
