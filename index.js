04ccb9220b20f812c93610e28f20e703262a0915
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-19 09:55:06 +0530

    file updated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const connectDB = require('./db/connectdb.js');
 const fs = require('fs');
 const server = express();
-const morgan = require('morgan');
 const baseURL = '/api/v1';
 const productURL = '/products';
 const userURL = '/users';
@@ -22,7 +21,6 @@ const cors = require('cors');
 // Server Middleware
 // server.use(cors());
 server.use(express.json());
-server.use(morgan('dev'));
 server.use(baseURL+productURL, productRouter.router);
 server.use(baseURL+userURL, userRouter.router);
 server.use(baseURL+orderURL, orderRouter.router);
